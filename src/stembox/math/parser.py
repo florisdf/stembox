62c77258b97558f0e024ea0036b697aa2ebf6424
Floris Odroid <floris_defeyter@hotmail.com>
2019-11-23 12:19:26 +0100

    Create parse function

---
+++ src/stembox/math/parser.py
@@ -0,0 +1,12 @@
+"""Module for parsing a string into an Expression object."""
+from ..math.expression import Expression
+
+
+def parse(string: str) -> Expression:
+    """Parse the given string into an Expression
+
+    :param str string: the string to parse
+    :returns: an Expression with the parsed Expression
+    :rtype: Expression
+    """
+    # Group
