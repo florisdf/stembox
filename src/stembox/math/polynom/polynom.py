f97b221685e9240427bee295d7a49ddb949cfd0f
florisdf <floris.defeyter@kuleuven.be>
2019-11-29 21:16:26 +0100

    Move solvers outside of Monomial class

--- src/stembox/math/polynom/polynom.py
+++ src/stembox/math/polynom/polynom.py
@@ -12,149 +12,159 @@ class Monomial(Expression):
     def __init__(self, factors: List[Factor]):
         self.factors = factors
 
-    def _get_var_paths(self) -> List[str]:
-        vars = []
-        for i, f in enumerate(self.factors):
-            path = '$.factors'
-            if isinstance(f, Variable):
-                path += f'[{i}]'
-            elif isinstance(f, Power) and isinstance(f.base, Variable):
-                path += f'[{i}].base'
-            elif isinstance(f, Fraction) and isinstance(f.numerator, Variable):
-                path += f'[{i}].numerator'
-            else:
-                continue
-            vars.append(path)
-        return vars
 
-    def _get_vars(self) -> List[Variable]:
-        return [self.get_at_path(path)
-                for path in self._get_var_paths()]
+def _get_var_paths(monomial) -> List[str]:
+    vars = []
+    for i, f in enumerate(monomial.factors):
+        path = '$.factors'
+        if isinstance(f, Variable):
+            path += f'[{i}]'
+        elif isinstance(f, Power) and isinstance(f.base, Variable):
+            path += f'[{i}].base'
+        elif isinstance(f, Fraction) and isinstance(f.numerator, Variable):
+            path += f'[{i}].numerator'
+        else:
+            continue
+        vars.append(path)
+    return vars
 
-    def _get_no_expon_var_paths(self) -> List[str]:
-        """Return the paths of the variables that lack an exponent."""
-        return [p for p in self._get_var_paths()
-                if not p.endswith('.base')]
 
-    def _get_no_expon_vars(self) -> List[Variable]:
-        """Return the variables that lack an exponent."""
-        return [self.get_at_path(path)
-                for path in self._get_no_expon_var_paths()]
+def _get_vars(monomial) -> List[Variable]:
+    return [monomial.get_at_path(path)
+            for path in _get_var_paths(monomial)]
 
-    def solve_vars(self) -> Solution:
-        # PURPOSE
-        purpose = Explanation(description=xpl.SEARCH_MONOM_VARS_PURP_DESCR())
 
-        # EXECUTION
-        monom_exe = deepcopy(self)
+def _get_no_expon_var_paths(monomial) -> List[str]:
+    """Return the paths of the variables that lack an exponent."""
+    return [p for p in _get_var_paths(monomial)
+            if not p.endswith('.base')]
 
-        for i, var in enumerate(monom_exe._get_vars()):
-            var.label = xpl.SEARCH_MONOM_VARS_EXEC_LABEL(i + 1)
-            var.mark = i
 
-        execution = Explanation(illustration=monom_exe)
+def _get_no_expon_vars(monomial) -> List[Variable]:
+    """Return the variables that lack an exponent."""
+    return [monomial.get_at_path(path)
+            for path in _get_no_expon_var_paths(monomial)]
 
-        # RESULT
-        monom_res = deepcopy(monom_exe)
-        for var in monom_res._get_vars():
-            var.label = None
 
-        result = Explanation(
-            description=xpl.SEARCH_MONOM_VARS_RESULT_DESCR(
-                monom_res._get_var_paths()),
-            illustration=monom_res)
+def solve_vars(monomial) -> Solution:
+    """Return the solution for finding the variables in the given monomial.
+    """
+    # PURPOSE
+    purpose = Explanation(description=xpl.SEARCH_MONOM_VARS_PURP_DESCR())
 
-        return Solution([Step(purpose=purpose,
-                              execution=execution,
-                              result=result)])
+    # EXECUTION
+    monom_exe = deepcopy(monomial)
 
-    def solve_unit_var_exponent(self) -> Solution:
-        """
-        Return the solution for explicitely adding a unit exponent to all
-        variables that do not have an exponent.
+    for i, var in enumerate(_get_vars(monom_exe)):
+        var.label = xpl.SEARCH_MONOM_VARS_EXEC_LABEL(i + 1)
+        var.mark = i
 
-        After this, the variables in `self` will all contain an exponent.
-        """
-        no_expon_vars = self._get_no_expon_var_paths()
+    execution = Explanation(illustration=monom_exe)
 
-        if len(no_expon_vars) == 0:
-            # All variables have an exponent, so return a Solution without any
-            # steps
-            return Solution()
+    # RESULT
+    monom_res = deepcopy(monom_exe)
+    for var in _get_vars(monom_res):
+        var.label = None
 
-        # PURPOSE
-        purpose = Explanation(
-            description=xpl.UNIT_VAR_EXPONENT_PURP_DESCR()
-        )
+    result = Explanation(
+        description=xpl.SEARCH_MONOM_VARS_RESULT_DESCR(
+            _get_var_paths(monom_res)),
+        illustration=monom_res)
 
-        # EXECUTION
-        for i, var in enumerate(no_expon_vars):
-            var.mark = i
+    return Solution([Step(purpose=purpose,
+                          execution=execution,
+                          result=result)])
 
-        # Copy the monomial and, in the old monomial, label the variables that
-        # do not have an exponent
-        old_monom = deepcopy(self)
-        for i, p in enumerate(old_monom._get_no_expon_var_paths()):
-            var = old_monom.get_at_path(p)
-            var.label = xpl.UNIT_VAR_EXPONENT_EXEC_LABEL()
 
-        # In the new monomial, i.e. `self`, wrap Powers around variables
-        for i, p in enumerate(self._get_no_expon_var_paths()):
-            var = self.get_at_path(p)
-            self.set_at_path(p, Power(base=var, exponent=Number(1)))
+def solve_unit_var_exponent(monomial) -> Solution:
+    """
+    Return the solution for explicitely adding a unit exponent to all
+    variables that do not have an exponent.
+    """
+    no_expon_vars = _get_no_expon_var_paths(monomial)
 
-        execution = Explanation(
-            description=xpl.UNIT_VAR_EXPONENT_EXEC_DESCR(),
-            illustration=Equivalence([old_monom, deepcopy(self)])
-        )
+    if len(no_expon_vars) == 0:
+        # All variables have an exponent, so return a Solution without any
+        # steps
+        return Solution()
 
-        # RESULT
-        result = Explanation(
-            decription=xpl.UNIT_VAR_EXPONENT_RESULT_DESCR(),
-            illustration=deepcopy(self))
+    # PURPOSE
+    purpose = Explanation(
+        description=xpl.UNIT_VAR_EXPONENT_PURP_DESCR()
+    )
 
-        return Solution([Step(purpose=purpose, execution=execution,
-                              result=result)])
+    # EXECUTION
+    for i, var in enumerate(no_expon_vars):
+        var.mark = i
 
-    def solve_var_expons(self) -> Solution:
-        """Return the solution for finding the exponents of each variable"""
-        # 1. Write 1 as exponent of variables with no exponent
-        solution = self.solve_unit_var_exponent()
+    # Copy the monomial and, in the old monomial, label the variables that
+    # do not have an exponent
+    old_monom = deepcopy(monomial)
+    for i, p in enumerate(_get_no_expon_var_paths(old_monom)):
+        var = old_monom.get_at_path(p)
+        var.label = xpl.UNIT_VAR_EXPONENT_EXEC_LABEL()
 
-        # 2. Mark the exponents
-        var_paths = self._get_var_paths()
+    # In the new monomial, i.e. `monomial`, wrap Powers around variables
+    for i, p in enumerate(_get_no_expon_var_paths(monomial)):
+        var = monomial.get_at_path(p)
+        monomial.set_at_path(p, Power(base=var, exponent=Number(1)))
 
-        # PURPOSE
-        purpose = Explanation(
-            description=xpl.SEARCH_VAR_EXPON_PURP_DESCR(len(var_paths)))
+    execution = Explanation(
+        description=xpl.UNIT_VAR_EXPONENT_EXEC_DESCR(),
+        illustration=Equivalence([old_monom, deepcopy(monomial)])
+    )
 
-        # EXECUTION
-        var_expons = {}
-        monom_exe = deepcopy(self)
-        for i, var_path in enumerate(var_paths):
-            if var_path.endswith('.base'):
-                pow_path = var_path[:var_path.rfind('.base')]
-                power = eval(f'{self}{pow_path[1:]}')
-                power.exponent.mark = power.base.mark
-                power.exponent.label = xpl.SEARCH_VAR_EXPON_EXEC_LABEL(
-                    var_path)
-                var_expons[var_path] = pow_path
-        execution = Explanation(illustration=monom_exe)
+    # RESULT
+    result = Explanation(
+        decription=xpl.UNIT_VAR_EXPONENT_RESULT_DESCR(),
+        illustration=deepcopy(monomial))
 
-        # RESULT
-        result = Explanation(
-            description=xpl.SEARCH_VAR_EXPON_RESULT_DESCR(var_expons),
-            illustration=deepcopy(monom_exe))
+    return Solution([Step(purpose=purpose, execution=execution,
+                          result=result)])
 
-        solution.append(Step(purpose=purpose,
-                             execution=execution,
-                             result=result))
-        return solution
 
-    def solve_grade(self) -> Solution:
-        # Find variables
-        # var_sol = self.solve_vars()
+def solve_var_expons(monomial) -> Solution:
+    """Return the solution for finding the exponents of each variable"""
+    # 1. Write 1 as exponent of variables with no exponent
+    solution = solve_unit_var_exponent(monomial)
 
-        # Find exponents of variables
-        # Sum exponents
-        pass
+    # 2. Mark the exponents
+    if len(solution) > 0:
+        monomial = deepcopy(solution[-1].result.illustration)
+    var_paths = _get_var_paths(monomial)
+
+    # PURPOSE
+    purpose = Explanation(
+        description=xpl.SEARCH_VAR_EXPON_PURP_DESCR(len(var_paths)))
+
+    # EXECUTION
+    var_expons = {}
+    monom_exe = deepcopy(monomial)
+    for i, var_path in enumerate(var_paths):
+        if var_path.endswith('.base'):
+            pow_path = var_path[:var_path.rfind('.base')]
+            power = eval(f'{monomial}{pow_path[1:]}')
+            power.exponent.mark = power.base.mark
+            power.exponent.label = xpl.SEARCH_VAR_EXPON_EXEC_LABEL(
+                var_path)
+            var_expons[var_path] = pow_path
+    execution = Explanation(illustration=monom_exe)
+
+    # RESULT
+    result = Explanation(
+        description=xpl.SEARCH_VAR_EXPON_RESULT_DESCR(var_expons),
+        illustration=deepcopy(monom_exe))
+
+    solution.append(Step(purpose=purpose,
+                         execution=execution,
+                         result=result))
+    return solution
+
+
+def solve_grade(monomial) -> Solution:
+    # Find variables
+    # var_sol = solve_vars(monomial)
+
+    # Find exponents of variables
+    # Sum exponents
+    pass
